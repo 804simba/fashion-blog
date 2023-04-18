6fb256a24484eee6df5d22e1636f7888ba052548
timolisa <timothyolisaeloka@gmail.com>
2023-04-18 11:47:49 +0100

    implement the like service and controller layers

--- src/main/java/com/timolisa/fashionblogapi/dto/LikeDTO.java
+++ src/main/java/com/timolisa/fashionblogapi/dto/LikeDTO.java
@@ -1,4 +1,12 @@
 package com.timolisa.fashionblogapi.dto;
 
+import com.timolisa.fashionblogapi.entity.Post;
+import com.timolisa.fashionblogapi.entity.User;
+import lombok.Data;
+
+@Data
 public class LikeDTO {
+    private Long numberOfLikes = 0L;
+    private User user;
+    private Post post;
 }
